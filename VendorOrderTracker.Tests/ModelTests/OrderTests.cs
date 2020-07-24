88d829f472ca53c5d25445c7aa649893814c1b0d
Peter Grimm <pagrimm@gmail.com>
2020-07-24 10:34:51 -0700

    add failing tests for get/set order object properties

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -9,12 +9,63 @@ namespace VendorOrderTracker.Tests
   public class OrderTests
   {
     [TestMethod]
-    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("some title", "some description", 100);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void GetTitle_ReturnsTitle_String()
+    {
+      string testTitle = "testTitle";
+      Order newOrder = new Order(testTitle, "some description", 100);
+      Assert.AreEqual(testTitle, newOrder.Title);
+    }
 
+    [TestMethod]
+    public void SetTitle_SetsTitle_String()
+    {
+      string testTitle = "testTitle";
+      Order newOrder = new Order(testTitle, "some description", 100);
+      string testNewTitle = "testNewTitle";
+      newOrder.Title = testNewTitle;
+      Assert.AreEqual(testNewTitle, newOrder.Title);
+    }
 
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string testDescription = "testDescription";
+      Order newOrder = new Order ("some title", testDescription, 100);
+      Assert.AreEqual(testDescription, newOrder.Description);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetsDescription_String()
+    {
+      string testDescription = "testDescription";
+      Order newOrder = new Order("some title", testDescription, 100);
+      string testNewDescription = "testNewDescription";
+      newOrder.Description = testNewDescription;
+      Assert.AreEqual(testNewDescription, newOrder.Description);
+    }
+
+    [TestMethod]
+    public void GetPrice_ReturnsPrice_Int()
+    {
+      string testPrice = 100;
+      Order newOrder = new Order ("some title", "some description", testPrice);
+      Assert.AreEqual(testPrice, newOrder.Price);
+    }
+
+    [TestMethod]
+    public void SetPrice_SetsPrice_Int()
+    {
+      string testPrice = 100;
+      Order newOrder = new Order("some title", "some description", testPrice);
+      string testNewPrice = 200;
+      newOrder.Price = testNewPrice;
+      Assert.AreEqual(testNewPrice, newOrder.Price);
+    }
   }
 }
